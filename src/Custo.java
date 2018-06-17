2aed3080aa72256acdadec7f2e1fd01afb7e75c1
Paolo Rechia <paolorechia@gmail.com>
2018-06-17 14:51:50 -0300

    Comecando custo e objetivo

--- src/Custo.java
+++ src/Custo.java
@@ -1,13 +1,16 @@
 public class Custo {
 
 	private int analise;
-
 	private int projeto;
-
 	private int BD;
-
 	private int programacao;
-
 	private int teste;
 
+  public Custo(int analise, int proj, int bd, int prog, int teste){
+    this.analise = analise;
+    this.projeto = proj;
+    this.BD = bd;
+    this.progamacao = prog;
+    this.teste = teste;
+  }
 }
--- src/Objetivo.java
+++ src/Objetivo.java
@@ -1,13 +1,27 @@
 public class Objetivo {
 
+	private String nome;
+  private Custo custo;
 	private int estadoConclusao;
 
+  public Objetivo(){
+    estadoConclusao = 0;  
+  }
 	public void setaNome(String nome) {
-
+    this.nome = nome;
 	}
-
+  public void getNome(){
+    return nome;
+  }
 	public void setaCusto(int analise, int proj, int bd, int prog, int teste) {
-
+    this.custo = new Custo(analise, proj, bd, prog, teste);
 	}
-
+  public void getCusto(){
+    return custo;
+  }
+  public void terminou(){
+    if (estadoConclusao < 100){
+      return false;
+    }
+    else return true;
 }
