7c5b9096f9f89c8a4f6c16141595efc43dddaac1
Paolo Rechia <paolorechia@gmail.com>
2018-06-17 13:04:29 -0300

    Adicionando arquivo .csv

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -1,3 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
 public class Arquivo {
 
 	public void salvar(Problema[] p) {
